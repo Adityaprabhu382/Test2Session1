0c9e924fb232394b393015093c4fcb404783e6a4
Aditya Prabhu <93111124+Adityaprabhu382@users.noreply.github.com>
2022-03-17 04:17:22 +0000

    distance

--- p1original.c
+++ p1original.c
@@ -1,2 +1,28 @@
+#include <stdio.h>
+#include<math.h>
+void input(float*x1, float*x2, float*y1, float*y2)
+{
+  printf("Enter the value\n");
+  scanf("%f%f",x1,y1);
+  printf("enter the coordinates of second point");
+  scanf("%f%f",x2,y2);
+}
+void find_output(float x1, float x2, float y1,float y2, float*distance)
+{
+  *distance=sqrt(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+}
+void output(float x1, float x2, float y1, float y2, float distance)
+{
+  printf("therefore the distance between points are %f",distance);
+
+  
+}
+int main()
+{
+  float x1,x2,y1,y2,distance;
+  input(&x1,&x2,&y1,&y2);
+  find_output(x1,x2,y1,y2,&distance);
+  output(x1,x2,y1,y2,distance);
+  return 0; 
 
 }
